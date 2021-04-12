f2a8c2ce76f027f1d51fcdfcb75eb1efe40c2f40
Sharath <Sharus9587@gmail.com>
2021-04-12 15:21:43 +0530

    Added : Compare one number with other

--- compare.sh
+++ compare.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 num1=10
-num2=10
+num2=14
 if [ $num1 -eq $num2 ]
 	then
 	echo $num1 and $num2 are equal
+	elif [ $num1 -gt $num2 ]
+	then
+	echo $num1 is greater than $num2
 	else
-	echo $num1 and $num2 are not equal
+	echo $num1 is less than $num2
 fi
